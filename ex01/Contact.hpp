ed6bd0b5b33672abd6dfa1763630cad7bea373ed
noureh10 <noureh.it@outlook.com>
2024-07-01 12:46:31 +0200

    3 - Adding current progress of PhoneBook exercice

---
+++ ex01/Contact.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Contact.hpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nechaara <nechaara@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 15:19:38 by nechaara          #+#    #+#             */
+/*   Updated: 2024/06/26 15:31:50 by nechaara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CONTACT_HPP
+#define CONTACT_HPP
+
+#include "Includes.hpp"
+
+class Contact
+{
+	private:
+		std::string name;
+		std::string surname;
+		std::string phone_number;
+	public:
+}
+
+#endif
---
+++ ex01/PhoneBook.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nechaara <nechaara@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/26 15:19:10 by nechaara          #+#    #+#             */
+/*   Updated: 2024/06/26 15:25:22 by nechaara         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHONEBOOK_HPP
+#define PHONEBOOK_HPP
+
+#include <iostream>
+#include "Contact.hpp"
+#include "Includes.hpp"
+
+class PhoneBook
+{
+	private: 
+		
+}
+
+
+#endif
