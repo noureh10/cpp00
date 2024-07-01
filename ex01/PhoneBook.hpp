31590163fa777dc4d2dd8871d5678ab8c64a6d6f
noureh10 <noureh.it@outlook.com>
2024-07-01 17:13:10 +0200

    5 - Adding PhoneBook header file

--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -3,25 +3,26 @@
 /*                                                        :::      ::::::::   */
 /*   PhoneBook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nechaara <nechaara@student.s19.be>         +#+  +:+       +#+        */
+/*   By: nechaara <nechaara.student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 15:19:10 by nechaara          #+#    #+#             */
-/*   Updated: 2024/06/26 15:25:22 by nechaara         ###   ########.fr       */
+/*   Updated: 2024/07/01 17:08:44 by nechaara         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 
-#include <iostream>
 #include "Contact.hpp"
 #include "Includes.hpp"
 
 class PhoneBook
 {
 	private: 
-		
-}
-
+		Contact PhoneBook[8];
+	public:
+		void	setContact(Contact contact);
+		void	displaySpecificContact(Contact contact);
+};
 
 #endif
